2bb3769eecdb13d84e6559cd6ea4e9461d274bd3
2sexyfortshirt <whosdefirst@gmail.com>
2026-05-19 18:26:40 +0300

    tyu

--- myreactapp/src/pages/ForgotPassword.js
+++ myreactapp/src/pages/ForgotPassword.js
@@ -1,35 +1,75 @@
+
 import { useState } from "react";
 import api from "../api";
 
+import "./ForgotPassword.css";
+
 function ForgotPassword() {
+
   const [email, setEmail] = useState("");
 
   const handleSubmit = async () => {
+
     try {
-      await api.post("password-reset/", { email });
+
+      await api.post(
+        "password-reset/",
+        { email }
+      );
+
       alert("Check your email");
+
     } catch (err) {
-     console.log(err.response?.data);
+
+      console.log(err.response?.data);
+
       alert("Error");
     }
   };
 
   return (
-    <div>
-      <h2>Forgot Password</h2>
 
-      <input
-        type="email"
-        placeholder="Enter your email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+    <div className="forgot-overlay">
 
-      <button onClick={handleSubmit}>
-        Send reset link
-      </button>
+      <div className="forgot-modal">
+
+        <div className="forgot-form">
+
+          <h2>Forgot Password</h2>
+
+          <p className="forgot-subtitle">
+            Enter your email to reset password
+          </p>
+
+          <div className="input-group">
+
+            <input
+              type="email"
+              placeholder=" "
+              value={email}
+              onChange={(e) =>
+                setEmail(e.target.value)
+              }
+            />
+
+            <label>Email</label>
+
+          </div>
+
+          <button
+            className="forgot-btn"
+            onClick={handleSubmit}
+          >
+            Send Reset Link
+          </button>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
 
 export default ForgotPassword;
+
